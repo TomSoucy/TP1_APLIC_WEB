1064705f0ea384cef5fd63b9598e2b297489ca50
paullenoir <lenoirpaul76@gmail.com>
2020-02-18 11:43:03 -0500

    route ajout
    manque si admin

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilmController;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group( function(){
+    Route::post('film/create', 'FilmController@create');
 });
