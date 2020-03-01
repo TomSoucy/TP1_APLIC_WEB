fb30bd06eef6341bc646dd86c3f66d94d89f339a
TomSoucy123 <tiyo_tom@hotmail.com>
2020-03-01 14:43:42 -0500

    postman testing

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,14 @@ use Illuminate\Http\Request;
     Route::post('api/film','FilmController@store');
     Route::get('/admin', 'AdminController@index')->name('admin')->middleware('admin'); //A redefinir le AdminController@index.
     Route::get('/user', 'AdminController@index')->name('user')->middleware('user');  
-    Route::get('login', array('uses' => 'LoginController@showLogin'));
-    Route::post('login', array('uses' => 'LoginController@doLogin'));
-   /*  Route::get('/user', function () {
-        return new UserResource(User::find(1));
-    }); */
+    Route::get('login','LoginController@showLogin');
+    Route::post('login','LoginController@doLogin');
+    /* Route::get('films', 'FilmController@index'); //afficher tous les films
+    Route::post('film','FilmController@store'); //enregistrer un film
+    Route::get('film/{idFilm}/edit','FilmController@edit'); //consulter 1 film
+    Route::put('film/{idFilm}','FilmController@update'); //modification d'un film
+    Route::delete('film/{idFilm}','FilmController@destroy'); //suppression d'un film
+    Route::get('film/{idFilm}/acteur','FilmController@showActor'); //consultation de tous les acteurs d'un film
+    Route::get('film/find/{rating?}/{minLength?}/{maxLength?}/{word?}','FilmController@find'); //consulter plusieurs films selon plusieurs parametres */
 
     /* https://laravel.com/docs/5.8/api-authentication */
