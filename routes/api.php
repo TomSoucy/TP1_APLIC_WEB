86358f24c65fee07f461e64b8b4f2509e22ef4b9
TomSoucy123 <tiyo_tom@hotmail.com>
2020-03-03 09:50:48 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,24 +14,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-    Route::post('film','FilmController@store')->middleware('auth:api');//exemple pour protéger certaines routes(powerpoint de la prof)
-    /* Route::middleware('auth')->group( function(){
-        // mettre les routes ici
-        pour appliquer le middleware a plusieurs routes
-        } */
-  /*   Route::get('login/admin', 'LoginsController@redirectTo');//->name('admin')->middleware('admin'); 
-    Route::get('login/user', 'LoginsController@redirectTo');//->name('user')->middleware('user');  */ 
+ 
+    
     Route::get('login', 'LoginsController@login');
-    Route::put('login/{idCritic}','LoginController@update'); //pas sur de la route
+    Route::put('login/{idCritic}','LoginController@update'); 
     Route::post('film','LoginsController@addUser');
     Route::post('critic','CriticController@store');
     Route::get('login/showUser', 'LoginsController@showUser');
     
-    /* Route::post('film','FilmController@store'); //enregistrer un film
-    Route::get('film/{idFilm}/edit','FilmController@edit'); //consulter 1 film
-    Route::put('film/{idFilm}','FilmController@update'); //modification d'un film
-    Route::delete('film/{idFilm}','FilmController@destroy'); //suppression d'un film
-    Route::get('film/{idFilm}/acteur','FilmController@showActor'); //consultation de tous les acteurs d'un film
-    Route::get('film/find/{rating?}/{minLength?}/{maxLength?}/{word?}','FilmController@find'); */ //consulter plusieurs films selon plusieurs parametres */
-
-    /* https://laravel.com/docs/5.8/api-authentication */
