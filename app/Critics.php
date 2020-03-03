1d26e22f16d6b871ffcee8154b3554abae89f839
TomSoucy123 <tiyo_tom@hotmail.com>
2020-03-02 21:04:52 -0500

    critics model

--- app/Critics.php
+++ app/Critics.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Critics extends Model
 {
-    //
+    protected $table = 'critics';
+
+    public function critics(){
+        return $this->hasMany('App\Comment', 'id', 'user_id');
+
+        return $this->hasMany('App\Films', 'id', 'film_id');
+    }
 }
+
