ddfd02fc8d2770baba05cc2f1ed3aa5ac2447144
TomSoucy123 <tiyo_tom@hotmail.com>
2020-03-02 21:09:08 -0500

    roles modele

--- app/Roles.php
+++ app/Roles.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Roles extends Model
 {
-    //
+    protected $table = 'roles';
+
+    public function roles(){
+        return $this->hasMany('App\Users', 'id', 'id');
+    }
 }
